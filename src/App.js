34287222d614eebd703e770e03f37ba4ff227f66
yewon97 <dpdnjs9936@gmail.com>
2022-01-07 03:14:16 +0900

    todo에 필요한 form과 이벤트 리스너 등 생성하기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { useState } from 'react';
+
 function App() {
-  return <div></div>;
+  const [toDo, setToDo] = useState('');
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if(toDo === "") {
+      // 비어 있으면 함수를 실행시키지 않음
+      return;
+    }
+    setToDo(""); // input을 비워줌
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do..." />
+        <button>Add To Do</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
