774d310f5055bb35793b13ce441474a702b88513
yewon97 <dpdnjs9936@gmail.com>
2022-01-07 02:08:01 +0900

    useEffect - state가 변해도 리렌더링 되지 않음

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 
 function App() {
   // create-react-app을 써서 React.을 써줄 필요가 없음
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
-  console.log('call an api');
+  console.log('i run all the time');
+  const iRunOnlyOnce = () => {
+    console.log('I run only once.');
+  };
+  useEffect(iRunOnlyOnce, []);
   return (
     <div>
       <h1>{counter}</h1>
