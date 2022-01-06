2d96b4918816dfc240b16560a0fcb2d01c16a54a
yewon97 <dpdnjs9936@gmail.com>
2022-01-07 03:23:16 +0900

    todo를 todos에 array로 담기

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 
 function App() {
   const [toDo, setToDo] = useState('');
+  const [toDos, setToDos] = useState([]);
   const onChange = (event) => setToDo(event.target.value);
   const onSubmit = (event) => {
     event.preventDefault();
@@ -9,10 +10,12 @@ function App() {
       // 비어 있으면 함수를 실행시키지 않음
       return;
     }
+    setToDos((currentArray) => [toDo, ...currentArray]); 
     setToDo(""); // input을 비워줌
   };
   return (
     <div>
+      <h1>My To Dos ({toDos.length})</h1>
       <form onSubmit={onSubmit}>
         <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do..." />
         <button>Add To Do</button>
