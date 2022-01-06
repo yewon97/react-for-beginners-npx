b039c0884d9805516c371c88c462ad8bf07b0828
yewon97 <dpdnjs9936@gmail.com>
2022-01-07 03:04:27 +0900

    todo 만들기 초기 셋팅

--- src/App.js
+++ src/App.js
@@ -1,32 +1,5 @@
-import { useState, useEffect } from 'react';
-
-function Hello() {
-  // function 으로 쓰는 법
-  useEffect(function() {
-    console.log("hi :)");
-    return function () {
-      console.log("bye :(");
-    };
-  }, []);
-  // 화살표함수로 쓰는 법
-  // useEffect(() => {
-  //   console.log("hi :)");
-  //   return () => console.log("bye :(");
-  // }, []);
-  return <h1>Hello</h1>;
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
-  return (
-  <div>
-    { // JS 쓸 때는 중괄호 쓰는 거 기억하기!
-      showing ? <Hello /> : null
-    }
-    <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
-  </div>
-  );
+  return <div></div>;
 }
 
 export default App;
