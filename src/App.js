d4035c95d8b5a3fa9dc353fd89bd533d792bad78
yewon97 <dpdnjs9936@gmail.com>
2022-01-07 02:34:00 +0900

    useEffect를 사용해서 무언가 변화할 때 특정 코드를 실행할 수 있음

--- src/App.js
+++ src/App.js
@@ -5,15 +5,18 @@ function App() {
   const [keyword, setKeyword] = useState('');
   const onClick = () => setValue((prev) => prev + 1);
   const onChange = (event) => setKeyword(event.target.value);
-  console.log('i run all the time');
   useEffect(() => {
-    console.log('CALL THE API...');
+    console.log('I run only once.');
   }, []);
   useEffect(() => {
-    if(keyword !== "" && keyword.length > 6){
-      console.log('Search for', keyword);
-    }
+      console.log("I run when 'keyword' changes.");
   }, [keyword]);
+  useEffect(() => {
+      console.log("I run when 'counter' changes.");
+  }, [counter]);
+  useEffect(() => {
+    console.log("I run when keyword & counter changes.");
+  }, [keyword, counter]);
     return (
     <div>
       <input 
