55937ca1d17c20cb5ccabdeb61808452baf30840
yewon97 <dpdnjs9936@gmail.com>
2022-01-07 01:58:50 +0900

    복습 - useState로 숫자 카운팅하기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import Button from "./Button";
-import styles from './App.module.css';
+import {useState} from 'react';
 
 function App() {
+  // create-react-app을 써서 React.을 써줄 필요가 없음
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!!</h1>
-      <Button text={'Continue'} />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
