020e74b77fdf36cfc4c9f31c20bdd6d731412dd4
yewon97 <dpdnjs9936@gmail.com>
2022-01-07 02:04:40 +0900

    state가 변하면 모든게 다 같이 리렌더링됨

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   // create-react-app을 써서 React.을 써줄 필요가 없음
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
+  console.log('call an api');
   return (
     <div>
       <h1>{counter}</h1>
