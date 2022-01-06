8f6a2319025bcebe0233a371a0ad7ec4bce3f9c5
yewon97 <dpdnjs9936@gmail.com>
2022-01-07 02:10:08 +0900

    API를 딱 한번만 호출하고 다시는 호출하고 싶지 않을 때

--- src/App.js
+++ src/App.js
@@ -5,10 +5,9 @@ function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
   console.log('i run all the time');
-  const iRunOnlyOnce = () => {
-    console.log('I run only once.');
-  };
-  useEffect(iRunOnlyOnce, []);
+  useEffect(() => {
+    console.log('CALL THE API...');
+  }, []);
   return (
     <div>
       <h1>{counter}</h1>
