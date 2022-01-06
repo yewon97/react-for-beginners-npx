2d3ce0e113ee0a18bfcfa8b173939258d4a1607f
yewon97 <dpdnjs9936@gmail.com>
2022-01-07 02:17:10 +0900

    useEffect를 써야하는 이유 - 글자를 타이핑할 때마다 리렌더링됨

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
-  // create-react-app을 써서 React.을 써줄 필요가 없음
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState('');
   const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
   console.log('i run all the time');
   useEffect(() => {
     console.log('CALL THE API...');
   }, []);
   return (
     <div>
+      <input 
+        value={keyword} 
+        onChange={onChange} 
+        type="text" 
+        placeholder="Search here..." 
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
