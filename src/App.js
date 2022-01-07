fd40346f9c8e0947407980ab8af853a94d8e8520
yewon97 <dpdnjs9936@gmail.com>
2022-01-07 23:54:43 +0900

    coin tracker 만들기 초기 셋팅

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
 import { useState } from 'react';
 
 function App() {
-  const [toDo, setToDo] = useState('');
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === '') {
-      // 비어 있으면 함수를 실행시키지 않음
-      return;
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo(''); // input을 비워줌
-  };
+  const [loading, setLoading] = useState(true);
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do..." />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <h1>The Coins</h1>
+      {loading ? <strong>Loading...</strong> : null}
     </div>
   );
 }
