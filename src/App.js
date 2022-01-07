d7d36079aacb507f4e8ba96d523622b12c981d01
yewon97 <dpdnjs9936@gmail.com>
2022-01-07 22:23:29 +0900

    map 메서드로 todo리스트 목록 보여주기

--- src/App.js
+++ src/App.js
@@ -6,13 +6,14 @@ function App() {
   const onChange = (event) => setToDo(event.target.value);
   const onSubmit = (event) => {
     event.preventDefault();
-    if(toDo === "") {
+    if (toDo === '') {
       // 비어 있으면 함수를 실행시키지 않음
       return;
     }
-    setToDos((currentArray) => [toDo, ...currentArray]); 
-    setToDo(""); // input을 비워줌
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo(''); // input을 비워줌
   };
+  console.log(toDos);
   return (
     <div>
       <h1>My To Dos ({toDos.length})</h1>
@@ -20,6 +21,12 @@ function App() {
         <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do..." />
         <button>Add To Do</button>
       </form>
+      <hr />
+      <ul>
+        {toDos.map((item) => (
+          <li>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
