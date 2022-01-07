a7ede7fd3fa6d0465da671fc0ab9a5513c0900d9
yewon97 <dpdnjs9936@gmail.com>
2022-01-08 00:06:10 +0900

    fetch API을 이용해서 응답을 JSON 형태로 파싱하기

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
   const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    fetch('https://api.coinpaprika.com/v1/tickers')
+      .then((response) => response.json()) // response로 부터 json을 추출
+      .then((json) => console.log(json));
+    // 빈배열은 딱 한번만 실행 된다.
+  }, []);
   return (
     <div>
       <h1>The Coins</h1>
