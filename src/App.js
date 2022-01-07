805e4abc2ba766ab51d0c84091609b38ffb901c2
yewon97 <dpdnjs9936@gmail.com>
2022-01-08 01:28:20 +0900

    await-async 2번째 방법

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
   const getMovies = async () => {
-    const response = await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`);
-    const json = await response.json();
+    const json = await (await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`)).json();
     setMovies(json.data.movies);
     setLoading(false);
   };
   useEffect(() => {
     getMovies();
   }, []);
+  console.log(movies);
   return <div>{loading ? <h1>Loading...</h1> : null}</div>;
 }
 
