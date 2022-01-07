90b055b097393c55220ddb208dc7c8e5e37e18cd
yewon97 <dpdnjs9936@gmail.com>
2022-01-08 00:08:43 +0900

    coins란 변수에 코인들의 array 담기

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import { useState, useEffect } from 'react';
 
 function App() {
   const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([])
   useEffect(() => {
     fetch('https://api.coinpaprika.com/v1/tickers')
       .then((response) => response.json()) // response로 부터 json을 추출
-      .then((json) => console.log(json));
-    // 빈배열은 딱 한번만 실행 된다.
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
   }, []);
   return (
     <div>
