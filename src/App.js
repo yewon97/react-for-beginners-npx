b5573eada299dc94dd0778bbab73c655ffa02259
yewon97 <dpdnjs9936@gmail.com>
2022-01-08 00:17:57 +0900

    추출한 json에서 map으로 원하는 값 가져오기

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([])
+  const [coins, setCoins] = useState([]); // empty array로 만들어서 undefined가 반환되지 않게 해야함 -> coins.length에서 오류가 남
   useEffect(() => {
     fetch('https://api.coinpaprika.com/v1/tickers')
       .then((response) => response.json()) // response로 부터 json을 추출
@@ -13,8 +13,15 @@ function App() {
   }, []);
   return (
     <div>
-      <h1>The Coins</h1>
+      <h1>The Coins ({coins.length})개</h1>
       {loading ? <strong>Loading...</strong> : null}
+      <ul>
+        {coins.map((coin) => (
+          <li>
+            {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
