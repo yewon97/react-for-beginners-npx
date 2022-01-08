acd5a43c458ac871764a9175efad7640f688ff1a
yewon97 <dpdnjs9936@gmail.com>
2022-01-08 21:43:25 +0900

    경로에 id 추가해서 id값 받아와서 이동하기

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,11 +1,11 @@
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-function Movie({ coverImg, title, summary, genres }) {
+function Movie({ id, coverImg, title, summary, genres }) {
   return (
     <div>
       <img src={coverImg} alt={title} />
-      <h2><Link to="/movie">{title}</Link></h2>
+      <h2><Link to={`/movie/${id}`}>{title}</Link></h2>
       <p>{summary}</p>
       <ul>
         {genres && genres.map((g) => (
@@ -17,6 +17,7 @@ function Movie({ coverImg, title, summary, genres }) {
 }
 
 Movie.propTypes = {
+  id: PropTypes.number.isRequired,
   coverImg: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
