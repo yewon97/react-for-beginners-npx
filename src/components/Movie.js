cec191457f31b6b24432f6ca4f579ce139a1df70
yewon97 <dpdnjs9936@gmail.com>
2022-01-08 21:31:31 +0900

    페이지가 reload되지 않고 경로 이동하는 방법(Link 사용)

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,10 +1,11 @@
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 function Movie({ coverImg, title, summary, genres }) {
   return (
     <div>
       <img src={coverImg} alt={title} />
-      <h2><a href='/movie'>{title}</a></h2>
+      <h2><Link to="/movie">{title}</Link></h2>
       <p>{summary}</p>
       <ul>
         {genres && genres.map((g) => (
