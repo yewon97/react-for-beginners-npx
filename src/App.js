b2b38ebf50bd5aa70737ba83a0a8d9b39fbb5bf5
yewon97 <dpdnjs9936@gmail.com>
2022-01-08 21:26:56 +0900

    react-router-dom으로 경로 설정하기

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import { useEffect, useState } from 'react';
 import Movie from './components/Movie';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// BrowerRouter, HashRouter 2가지 종류가 있는데 HashRouter -> http://localhost:3000/#/movie -> #이 붙는 것이다.
+// 컴포넌트 import 까먹지 말기!
+import Home from './routes/Home';
+import Detail from './routes/Detail';
 
 function App() {
-  return null;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/movie">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
